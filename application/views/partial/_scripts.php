8beee22ca321b15824efb8490a66d1400d90acf7
eslam <eslam@yahoo.com>
2017-04-08 15:42:07 +0200

    update ui design

--- application/views/partial/_scripts.php
+++ application/views/partial/_scripts.php
@@ -1,4 +1,4 @@
 <script type="text/javascript" src="<?= base_url('assets/js/jquery-1.12.0.min.js'); ?>"></script>
-<script type="text/javascript" src="<?= base_url('assets/js/bootstrap.min.js'); ?>"></script>
+<script type="text/javascript" src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 <script type="text/javascript" src="<?= base_url('assets/js/script.js'); ?>"></script>
 
